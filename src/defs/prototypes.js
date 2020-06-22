3c42b6f5a3db55532037cc3dbc09433bccc994e3
Jan Randa <jnranda@gmail.com>
2020-06-22 11:47:32 +0200

    BF: jet.prototyper was enumerable

--- src/defs/prototypes.js
+++ src/defs/prototypes.js
@@ -1,5 +1,5 @@
 import jet from "../jet";
 
-jet.obj.map(jet.str, f=>jet.obj.addProperty(String.prototype, f.name, function(...args) {return f(this, ...args);}, false, true, false));
+jet.obj.map(jet.str, f=>jet.obj.addProperty(String.prototype, f.name, function(...args) {return f(this, ...args);}, false, false, false));
 
-jet.obj.map(jet.arr, f=>jet.obj.addProperty(Array.prototype, f.name, function(...args) {return f(this, ...args);}, false, true, false));
+jet.obj.map(jet.arr, f=>jet.obj.addProperty(Array.prototype, f.name, function(...args) {return f(this, ...args);}, false, false, false));
